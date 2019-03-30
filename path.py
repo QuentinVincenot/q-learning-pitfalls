c1218f0b4dc337106832c2bf99e202dde9a7f978
Quentin Vincenot <quentin.vincenot@gmail.com>
2019-03-30 18:29:31 +0100

    Factorised plotting functions

--- path.py
+++ path.py
@@ -10,14 +10,12 @@ def train_AI_for_n_episodes(agent, environment, fig, ax, episodes=10):
 
 def run_episode(agent, environment, fig, ax, training=False):
 	environment.reset()
-	scatterplot = plot_position(environment, ax)
-	plt.pause(0.1)
+	scatterplot = plot_position(ax, environment)
 	while(environment.is_game_finished() == False):
 		current_state = environment.current_state
 		next_action = agent.choose_action(current_state)
 		new_state, reward = agent.act_in_environment(environment, next_action)
-		update_position(environment, fig, scatterplot)
-		plt.pause(0.05)
+		update_position(fig, scatterplot, environment)
 		if training == True:
 			agent.update_q_table(environment, current_state, next_action, new_state, reward)
 	erase_position(scatterplot)
@@ -26,5 +24,5 @@ def run_episode(agent, environment, fig, ax, training=False):
 agent = Agent(learning_rate=0.5, actualisation_factor=0.9)
 environment = Environment()
 
-plt, fig, ax = initialise_plot()
+plt, fig, ax = initialise_plot(environment)
 train_AI_for_n_episodes(agent, environment, fig, ax, 100)
--- visualisation.py
+++ visualisation.py
@@ -1,37 +1,32 @@
 import matplotlib.pyplot as plt
 
-def initialise_plot():
+def initialise_plot(environment):
 	plt.ion()
 	fig = plt.figure(figsize=(10, 8))
 	ax = plt.gca()
 	plt.axes().set_aspect('equal')
 	plt.tick_params(axis='both', which='both', bottom='off', top='off', labelbottom='off', right='off', left='off', labelleft='off')
 	ax.axis('off')
-	plot_grid_and_color_floors(ax)
+	plot_grid_and_color_floors(ax, environment)
 	plt.show()
 	return plt, fig, ax
 
-def plot_grid_and_color_floors(ax):
-	plot_lines_and_contours(ax)
-	color_floor(ax, 0, 1, 0, 1, 'r', 0.2)
-	color_floor(ax, 1, 2, 0, 1, 'r', 0.2)
-	color_floor(ax, 2, 3, 0, 1, 'r', 0.2)
-	color_floor(ax, 0, 1, 1, 2, 'r', 0.2)
-	color_floor(ax, 1, 2, 1, 2, 'r', 0.7)
-	color_floor(ax, 2, 3, 1, 2, 'r', 0.2)
-	color_floor(ax, 0, 1, 2, 3, 'r', 0.2)
-	color_floor(ax, 1, 2, 2, 3, 'r', 0.7)
-	color_floor(ax, 2, 3, 2, 3, 'b', 0.7)
+def plot_grid_and_color_floors(ax, environment):
+	plot_lines_and_contours(ax, environment)
+	for reward in environment.rewards:
+		color = 'r' if environment.rewards[reward] < 0.0 else 'b'
+		alpha = 0.2 if abs(environment.rewards[reward]) < 10.0 else 0.7
+		color_floor(ax, reward[0], reward[0]+1, reward[1], reward[1]+1, color, alpha)
 
-def plot_lines_and_contours(ax):
-	plot_line_start_stop(ax, (0,0), (0,3))
-	plot_line_start_stop(ax, (0,3), (3,3))
-	plot_line_start_stop(ax, (3,3), (3,0))
-	plot_line_start_stop(ax, (3,0), (0,0))
-	plot_line_start_stop(ax, (1,0), (1,3))
-	plot_line_start_stop(ax, (2,0), (2,3))
-	plot_line_start_stop(ax, (0,1), (3,1))
-	plot_line_start_stop(ax, (0,2), (3,2))
+def plot_lines_and_contours(ax, environment):
+	minx = environment.current_state[0]
+	maxx = environment.final_state[0] + 1
+	miny = environment.current_state[1]
+	maxy = environment.final_state[1] + 1
+	for row in range(maxx+1):
+		plot_line_start_stop(ax, (minx, row), (maxx, row))
+	for col in range(maxy+1):
+		plot_line_start_stop(ax, (col, miny), (col, maxy))
 
 def plot_line_start_stop(ax, start, stop):
 	ax.plot([start[0],stop[0]], [start[1],stop[1]], linestyle="--", color="#000000")
@@ -39,14 +34,17 @@ def plot_line_start_stop(ax, start, stop):
 def color_floor(ax, minx, maxx, miny, maxy, color, alpha):
 	ax.fill([minx, maxx, maxx, minx], [miny, miny, maxy, maxy], color, alpha=alpha)
 
-def plot_position(environment, ax):
+def plot_position(ax, environment):
 	position = environment.current_state
-	return ax.scatter([position[0]+0.5], [position[0]+0.5], marker="o", c="g", s=250, alpha=1.0)
+	scatterplot = ax.scatter([position[0]+0.5], [position[0]+0.5], marker="o", c="g", s=250, alpha=1.0)
+	plt.pause(0.05)
+	return scatterplot
 
-def update_position(environment, fig, scatterplot):
+def update_position(fig, scatterplot, environment):
 	position = environment.current_state
 	scatterplot._offsets = [[position[0]+0.5, position[1]+0.5]]
 	fig.canvas.draw()
+	plt.pause(0.025)
 
 def erase_position(scatterplot):
 	scatterplot._offsets = []
